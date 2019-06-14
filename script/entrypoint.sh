eebc9bd635a75501312b5a0184fa8f47a29dd42a
Justin Palmer <encytemedia@gmail.com>
2019-06-14 16:18:08 -0700

    run every 60 seconds

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # while true; do sleep 15 ; echo "background"; done &
 
-while true; do sleep 12 ; python "urban-traffic/scheduler.py"; done
+while true; do python "urban-traffic/scheduler.py"; sleep 60 ; done
