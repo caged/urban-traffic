db9ac70296878f25ae9b68db7e029c7da51ab87f
Justin Palmer <encytemedia@gmail.com>
2019-06-14 16:18:01 -0700

    rough working pass

--- urban-traffic/scheduler.py
+++ urban-traffic/scheduler.py
@@ -1,6 +1,14 @@
-import time
+import os
+from datetime import datetime
+from traffic_api import TrafficApi
+
 
 def say_hello(message="Hello World"):
-        print(time.time(), message)
+    traffic = TrafficApi(app_id=os.environ['APP_ID'],
+                         app_code=os.environ['APP_CODE'])
+    data = traffic.flow_by_bbox(bbox=[45.391700,-122.826462,45.657728,-122.347183])
+    print(data.json())
+    print(datetime.now(), message)
+
 
 say_hello()
---
+++ urban-traffic/traffic_api.py
@@ -0,0 +1,42 @@
+import sys
+import requests
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+
+class TrafficApi:
+
+    def __init__(self, app_id, app_code, retries=5, backoff_factor=1,
+                 status_forcelist=[502, 503, 504],
+                 base_url='https://traffic.api.here.com/traffic'):
+        self.app_id = app_id
+        self.app_code = app_code
+        self.base_url = base_url
+
+        self.session = requests.Session()
+        retries = Retry(total=retries, backoff_factor=backoff_factor,
+                        status_forcelist=status_forcelist)
+        self.session.mount('https://', HTTPAdapter(max_retries=retries))
+        print('hello world')
+
+    def flow_by_bbox(self, bbox):
+        bbox_formatted = f"{bbox[0]},{bbox[1]}; {bbox[2]},{bbox[3]}"
+        print(bbox_formatted)
+        return self.__flow(params={'bbox': bbox_formatted})
+
+    def __flow(self, params={}):
+        return self.__get(f'{self.base_url}/6.2/flow.json', params)
+
+    def __get(self, url, params = {}):
+        default_params = {'app_id': self.app_id, 'app_code': self.app_code}
+        payload = {**default_params, **params}
+        # Hack to prevent escaping.  The HERE api can't handle url encoded
+        # strings and requests automatically escapes dictionary items.
+        payload_str = "&".join("%s=%s" % (k,v) for k,v in payload.items())
+        try:
+            r = requests.get(url, params=payload_str)
+            r.raise_for_status()
+            return r
+        except requests.exceptions.HTTPError as err:
+            print(err)
+            sys.exit(1)
