149093011869211a584a5d96114450d31a065c5d
Justin Palmer <encytemedia@gmail.com>
2019-06-14 22:04:56 -0700

    run every 10 seconds while debugging

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # while true; do sleep 15 ; echo "background"; done &
 
-while true; do python "urban-traffic/scheduler.py"; sleep 60 ; done
+while true; do python "urban-traffic/scheduler.py"; sleep 10 ; done
