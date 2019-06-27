c16b11a157b453460619ac361c29280a85b0c7de
Justin Palmer <encytemedia@gmail.com>
2019-06-27 11:00:40 -0700

    formatting

--- urban-traffic/scheduler.py
+++ urban-traffic/scheduler.py
@@ -5,13 +5,15 @@ from traffic_flow import TrafficFlow
 import json
 from xml.etree import ElementTree
 
+
 def say_hello(message="Hello World"):
     traffic = TrafficApi(app_id=os.environ['APP_ID'],
                          app_code=os.environ['APP_CODE'])
-    tf = traffic.flow_by_bbox(bbox=[45.391700,-122.826462,45.657728,-122.347183])
+    tf = traffic.flow_by_bbox(
+        bbox=[45.391700, -122.826462, 45.657728, -122.347183]
+    )
     for feature in tf.features():
         print(feature)
-
 
 
 say_hello()
